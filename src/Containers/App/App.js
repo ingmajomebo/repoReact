26d8c381405d24ae8d6516caaa6e8a29be3bd7a7
Wilson Javier Tobón Bedoya <wijar626@gmail.com>
2018-12-05 17:34:24 -0500

    master

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+              {this.props.title}
           </p>
           <a
             className="App-link"
@@ -24,5 +26,9 @@ class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default App;
